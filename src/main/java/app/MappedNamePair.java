b51fb50a511f53cbabcb19fbd7778ebb577dc83e
Renee Slamka <renee.slamka@gmail.com>
2015-09-10 17:34:20 -0700

    Added database components

---
+++ src/main/java/app/MappedNamePair.java
@@ -0,0 +1,26 @@
+package main.java.app;
+
+public class MappedNamePair {
+	private final String submittedName;
+	private final String gameOfThronesName;
+	
+	public MappedNamePair(String submittedName, String gameOfThronesName) {
+		this.submittedName = submittedName;
+		this.gameOfThronesName = gameOfThronesName;
+	}
+	
+	@Override
+    public String toString() {
+        return String.format(
+            "MappedNamePair[name='%s', lastName='%s']",
+            submittedName, gameOfThronesName);
+    }
+
+    public String getSubmittedName() {
+    	return submittedName;
+    }
+    
+    public String getGamoeOfThronesName() {
+    	return gameOfThronesName;
+    }
+}
